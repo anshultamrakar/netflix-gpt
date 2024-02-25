128b677ca28422c85357934f8b169be1baea1f91
Anshul Tamrakar <anshultamrakar487@gmail.com>
2024-02-25 19:58:08 +0530

    more changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-
+import Body from "./components/Body";
 
 const App = () =>  {
   return (
-    <div className="text-3xl font-bold underline text-green-800">
-     <h2>Hello World</h2>
-    </div>
+   <Body/>
   );
 }
 
---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from './Login'
+import Browser from './Browser'
+
+
+const Body = () => {
+  return (
+    <div>
+     <Login/>
+     <Browser/>
+    </div>
+  )
+}
+
+export default Body
